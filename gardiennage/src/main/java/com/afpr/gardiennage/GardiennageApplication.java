e8e4a356b9a69954a875de7ccc50454bd338c539
itlili <itlili@host.docker.internal>
2021-11-16 14:44:51 +0100

    1er commit

---
+++ gardiennage/src/main/java/com/afpr/gardiennage/GardiennageApplication.java
@@ -0,0 +1,13 @@
+package com.afpr.gardiennage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GardiennageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GardiennageApplication.class, args);
+	}
+
+}
---
+++ gardiennage/src/test/java/com/afpr/gardiennage/GardiennageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.afpr.gardiennage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GardiennageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
